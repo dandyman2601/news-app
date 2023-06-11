338a9a17e7718728c78622519331b2165adaf20d
Alvin Ajay <33617453+dandyman2601@users.noreply.github.com>
2023-06-12 00:15:33 +0530

    add detail view and router scaffolding

---
+++ src/views/DetailView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Detail page {{ $route.params.id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
