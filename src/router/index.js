41d83f45acf396e0fb140346028c1874a30cac86
Alvin Ajay <33617453+dandyman2601@users.noreply.github.com>
2023-06-12 21:47:49 +0530

    feat: add Detail page component

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import ListView from "../views/ListView.vue";
+import DetailView from "../views/DetailView.vue";
 
 Vue.use(VueRouter);
 
@@ -8,7 +9,7 @@ const routes = [
   {
     path: "/",
     name: "home",
-    component: HomeView,
+    component: ListView,
   },
   {
     path: "/:id",
@@ -16,7 +17,8 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/DetailView.vue"),
+    component: DetailView,
+    props: true,
   },
 ];
 
--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -1,14 +1,53 @@
 <template>
-  <div>
-    <h1>Detail page {{ $route.params.id }}</h1>
+  <div class="ma-10 mx-lg-auto">
+    <div class="mx-10 my-5">
+      <v-icon class="mb-5" color="black" size="50" @click="$router.back()">mdi-arrow-left</v-icon>
+      <h1 class="text-center text-xl">{{ detail.title }}</h1>
+      <h4 class="text-xl text-center my-5 grey--text caption">
+        {{ detail.description }}
+      </h4>
+      <h5 v-if="detail.author">By {{ detail.author }}</h5>
+    </div>
+    <div class="mx-10 my-5">
+      <v-img class="my-5" :src="detail.urlToImage" height="100%" cover></v-img>
+
+      <!-- Drop Cap effect -->
+      <p v-if="detail.content" class="body first-letter">{{ detail.content }}</p>
+      <p class="body text-center text-xl" v-else>
+        :( <br />
+        No Content Found
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  name: "DetailComponent",
   data() {
-    return {};
+    return {
+      detail: {},
+      index: null,
+    };
   },
-  methods: {},
+  created() {
+    this.detail = this.$route.params.data;
+    this.index = this.$route.params.id;
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+// Drop cap effect
+.first-letter::first-letter {
+  padding: 0 0.3rem;
+  margin: 0 0.3rem 0 0;
+  border: 2px solid;
+  border-radius: 8px;
+  font-weight: 900;
+  font-style: italic;
+  font-size: 4rem;
+  float: left;
+  line-height: 1;
+}
+</style>
