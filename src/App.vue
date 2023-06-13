ccc186f6b9adf88cf0e698fecb4c8db8d3d5f555
Alvin Ajay <33617453+dandyman2601@users.noreply.github.com>
2023-06-13 23:09:07 +0530

    feat: add nagivation drawer

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,101 @@
 <template>
   <v-app>
-    <v-main>
-      <v-toolbar>
-        <v-btn icon>
-          <v-icon>mdi-menu</v-icon>
-        </v-btn>
-        <v-spacer></v-spacer>
-        <v-toolbar-title>
-          <h2 class="logo">AlvinDaily</h2>
-        </v-toolbar-title>
-        <v-spacer></v-spacer>
+    <v-navigation-drawer v-model="drawer" app>
+      <!-- Drawer content -->
+      <v-list>
+        <v-list-item v-for="(item, index) in drawerItems" :key="index">
+          <!-- @click="navigateTo(item.route)" -->
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>
+            <v-btn @click="clickDrawerItem(item)" plain :class="item.style" class="overline pa-0">{{
+              item.title
+            }}</v-btn>
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
+      <v-toolbar-title>
+        <h2 class="logo" @click="goBackHome" @keydown.enter="goBackHome" tabindex="0" role="button">
+          The AlvinDaily
+        </h2>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon @click="openDialog('search')">
         <v-icon>mdi-magnify</v-icon>
-      </v-toolbar>
-      <router-view></router-view>
+      </v-btn>
+    </v-app-bar>
+    <Dialog :dialog-mode="mode" :dialog="searchDialog" @close-dialog="closeDialog" />
+    <v-main>
+      <!-- :key helps rerender while on same view -->
+      <router-view :key="$route.fullPath"></router-view>
     </v-main>
   </v-app>
 </template>
+
 <script>
 export default {
   data() {
-    return {};
+    return {
+      drawer: false,
+      searchDialog: false,
+      drawerItems: [
+        {
+          title: "History",
+          icon: "mdi-history",
+          mode: "history",
+          style: "",
+        },
+        {
+          title: "Make Error Call",
+          icon: "mdi-alert-circle",
+          mode: "trigger",
+          style: "red--text",
+        },
+      ],
+      mode: "search",
+    };
   },
-  created() {},
+  methods: {
+    goBackHome() {
+      this.$router.push({ name: "home" }).catch((err) => console.warn(err));
+    },
+    navigateTo(route) {
+      this.$router.push(route);
+      this.drawer = false; // Close the drawer after navigation
+    },
+    clickDrawerItem(item) {
+      this.mode = item.mode;
+      if (this.mode !== "trigger") {
+        this.openDialog(this.mode);
+      }
+    },
+    openDialog(mode) {
+      this.mode = mode;
+      this.searchDialog = true;
+    },
+    closeDialog() {
+      this.searchDialog = false;
+    },
+  },
+  components: {
+    Dialog: () => import("@/components/Dialog.vue"),
+  },
 };
 </script>
+
 <style lang="scss">
 @import "./scss/variables.scss";
 v-app {
   font-family: $body-font-family !important;
+}
+.logo {
+  font-size: 24px;
+  font-weight: bold;
 }
 </style>
